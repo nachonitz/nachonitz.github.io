b8a5f1cb30edf3e4001f5156189aa75a2929312e
Ignacio Nitz <33585367+nachonitz@users.noreply.github.com>
2021-03-12 19:20:12 -0300

    Add files via upload

--- tp2Entrega/shaders/vertex-shader-tierra.glsl
+++ tp2Entrega/shaders/vertex-shader-tierra.glsl
@@ -36,60 +36,144 @@
 
         const float amplitud=8.0;
 
+
+        // controla la cantidad de muestras que se promedian
+
+        const int samplingRange=3; // 0 = 1 muestra,  1 = 9 muestras, 2= 25 muestras, 3 = 49 muestras
+        
+        // toma un promedio ponderados de muestras de una textura
+
+        float multisample(sampler2D texture,vec2 coord){
+
+
+            float sum=0.0;
+            float totalWeight;
+            float pixelDistance=(1.0/1024.);
+
+            for (int i=-samplingRange;i<=samplingRange;i++){
+                for (int j=-samplingRange;j<=samplingRange;j++){
+
+                    float weight=1.0/(1.0+sqrt(pow(float(j),2.0)+pow(float(i),2.0)));
+                    totalWeight+=weight;
+
+                    vec2 uv=coord+vec2(float(i),float(j))*pixelDistance*2.0;
+                    sum+=weight*texture2D(texture, vec2(uv.s, uv.t)).x;
+                }
+            }
+
+            return sum/totalWeight;
+        }
+
+
         void main(void) {
                     
-            vec3 position = aPosition;		
-            vec3 normal = aNormal;	
-            vec2 uv = aUv;
+            // vec3 position = aPosition;		
+            // vec3 normal = aNormal;	
+            // vec2 uv = aUv;
                                    	
-            vec4 center = texture2D(uSampler, vec2(uv.s, uv.t));                     
-            vec4 masU = texture2D(uSampler, vec2(uv.s+epsilon, uv.t));  
-            vec4 masV = texture2D(uSampler, vec2(uv.s, uv.t+epsilon));  
+            // vec4 center = texture2D(uSampler, vec2(uv.s, uv.t));                     
+            // vec4 masU = texture2D(uSampler, vec2(uv.s+epsilon, uv.t));  
+            // vec4 masV = texture2D(uSampler, vec2(uv.s, uv.t+epsilon));  
 
-            vec4 menosU = texture2D(uSampler, vec2(uv.s-epsilon, uv.t));  
-            vec4 menosV = texture2D(uSampler, vec2(uv.s, uv.t-epsilon));  
-
-
-            // elevamos la coordenada Y
-            position.y+=-1.5 + center.x*amplitud;
-
-            vec4 worldPos = uMMatrix*vec4(position, 1.0);                        
-
-            gl_Position = uPMatrix*uVMatrix*worldPos;
-
-            vWorldPosition=worldPos.xyz;              
-            
-            // tangentes en U y en V
-            // vec3 gradU1=vec3(0.,(masU.x-center.x)*amplitud,epsilon);
-            // vec3 gradV1=vec3(epsilon,(masV.x-center.x)*amplitud,0.0);
-
-            // vNormal=normalize(cross(gradV1,gradU1));
-
-            float angU=atan((masU.x-center.x)*amplitud,epsilon);
-            float angV=atan((masV.x-center.x)*amplitud,epsilon);
-
-            // tangentes en U y en V
-            vec3 gradU1=vec3(cos(angU),sin(angU),0.0);
-            vec3 gradV1=vec3(0.0      ,sin(angV),cos(angV));
-            
-            angU=atan((center.x-menosU.x)*amplitud,epsilon);
-            angV=atan((center.x-menosV.x)*amplitud,epsilon);
-
-            // segundo conjunto de tangentes en U y en V
-            vec3 gradU2=vec3(cos(angU),sin(angU),0.0);
-            vec3 gradV2=vec3(0.0      ,sin(angV),cos(angV));
+            // vec4 menosU = texture2D(uSampler, vec2(uv.s-epsilon, uv.t));  
+            // vec4 menosV = texture2D(uSampler, vec2(uv.s, uv.t-epsilon));  
 
 
-            
-            // calculo el producto vectorial
-            vec3 tan1=(gradV1+gradV2)/2.0;
-            vec3 tan2=(gradU1+gradU2)/2.0;
-            vNormal=cross(tan1,tan2);
+            // // elevamos la coordenada Y
+            // position.y+=-1.5 + center.x*amplitud;
 
-
-
-            vectorPuntoCamara = normalize(-vec3(uVMatrix*worldPos) / (uVMatrix*worldPos).w);                  
+            // vec4 worldPos = uMMatrix*vec4(position, 1.0);                        
 
             // gl_Position = uPMatrix*uVMatrix*worldPos;
-            vUv=uv;	
+
+            // vWorldPosition=worldPos.xyz;              
+            
+            // // tangentes en U y en V
+            // // vec3 gradU1=vec3(0.,(masU.x-center.x)*amplitud,epsilon);
+            // // vec3 gradV1=vec3(epsilon,(masV.x-center.x)*amplitud,0.0);
+
+            // // vNormal=normalize(cross(gradV1,gradU1));
+
+            // float angU=atan((masU.x-center.x)*amplitud,epsilon);
+            // float angV=atan((masV.x-center.x)*amplitud,epsilon);
+
+            // // tangentes en U y en V
+            // vec3 gradU1=vec3(cos(angU),sin(angU),0.0);
+            // vec3 gradV1=vec3(0.0      ,sin(angV),cos(angV));
+            
+            // angU=atan((center.x-menosU.x)*amplitud,epsilon);
+            // angV=atan((center.x-menosV.x)*amplitud,epsilon);
+
+            // // segundo conjunto de tangentes en U y en V
+            // vec3 gradU2=vec3(cos(angU),sin(angU),0.0);
+            // vec3 gradV2=vec3(0.0      ,sin(angV),cos(angV));
+
+
+            
+            // // calculo el producto vectorial
+            // vec3 tan1=(gradV1+gradV2)/2.0;
+            // vec3 tan2=(gradU1+gradU2)/2.0;
+            // vNormal=cross(tan1,tan2);
+
+
+
+            // vectorPuntoCamara = normalize(-vec3(uVMatrix*worldPos) / (uVMatrix*worldPos).w);                  
+
+            // // gl_Position = uPMatrix*uVMatrix*worldPos;
+            // vUv=uv;
+
+
+                    
+            vec3 position = aPosition;		            
+            vec2 uv = aUv;
+
+            float epsilon=1.0/1024.;
+            
+            float center = multisample(uSampler, vec2(uv.s, uv.t));
+    
+            float centerMasX = multisample(uSampler, vec2(uv.s+epsilon, uv.t));  
+            float centerMasZ = multisample(uSampler, vec2(uv.s, uv.t+epsilon));  
+
+            // elevamos la coordenada Y
+            position.y+=-1.5+center*amplitud;
+
+            vec4 worldPos = uMMatrix*vec4(position, 1.0);                        
+            gl_Position = uPMatrix*uVMatrix*worldPos;
+
+            vWorldPosition=worldPos.xyz;        
+
+            /*
+              hay que calcular la normal de la superficie
+
+              La idea es calcular la diferencia de altura entre 2 puntos muy prÃ³ximos (separados por Epsilon)
+              obtener la pendiente  y estimar el vector tangente en el sentido del eje "X" y el eje "Z" (+Y es arriba)
+
+
+                    * - - - - -  - - - - - - - - - - - }
+                    |                                  }  deltaElevationX
+                    |                       *  - - - - }
+                    |                       |
+                ----|-----------------------|--------> +X
+                    * center               * centerMasX
+
+                    < -      epsilon     - >
+            */
+
+            // diferencias de elevaciÃ³n entre 2 puntos proximos
+            
+            float deltaElevationX=(centerMasX-center)*amplitud;
+            float deltaElevationZ=(centerMasZ-center)*amplitud;
+  
+
+            // angulo del vector tangente en el plano XY, ZY respectivamente            
+            float angEnX=atan(deltaElevationX,epsilon);
+            float angEnZ=atan(deltaElevationZ,epsilon);
+
+            // vectores tangentes
+            vec3 tangenteX=vec3(cos(angEnX),sin(angEnX),0.0);
+            vec3 tangenteZ=vec3(0.0      ,sin(angEnZ),cos(angEnZ));
+
+            // vector normal
+            vNormal=cross(tangenteZ,tangenteX);
+            vUv=uv;		
         }
